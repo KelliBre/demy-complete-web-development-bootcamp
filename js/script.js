342f4db4fa4a707a8c4ec5a505a2aa046a4235f6
KelliBre <Kelli.Brecunier@gmail.com>
2022-10-14 12:47:44 +1000

    Added Twitter challenge

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-//Section 102
+/*Section 102
 alert("hello world");
+
+//Section 109
+
+let myName = prompt("What is your name?");
+let nameLength = myName.length;
+console.log(nameLength);
+
+*/
+//twitter challange
+let userInput = prompt("Enter your Tweet");
+let charLength = userInput.length 
+let remainingChar = 140 - charLength ;
+alert("You have used " + charLength + " characters." + "You have " + remainingChar + " characters remaining!");
+
+console.log(remainingChar);
+
+//secti
 
